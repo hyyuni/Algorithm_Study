dc650d8be564b2083355bcbd90c310ce2a8478e5
hyyuni <cjn112070@gmail.com>
2025-02-08 22:16:48 +0900

    [SEA] 스도쿠 검증 / 1974 / D2 / 60분

--- 1week/hyun/sea_1974.py
+++ 1week/hyun/sea_1974.py
@@ -1,20 +1,23 @@
+# 스도쿠 퍼즐을 검사하기 위한 함수 생성
+def checklist(arr):
+    sort_list = list(range(1,10))
+    check_list = arr[:]
+    check_list.sort()
+    return True if sort_list == check_list else False
+
 T = int(input())
 for tc in range(1, T+1):
     # 스도쿠 퍼즐을 리스트 컴프리헨션으로 생성
     arr = [list(map(int, input().split())) for _ in range(9)]
-    ans = 0
-    # 스도쿠 퍼즐을 검사하기 위한 함수 생성
-    def checklist(arr):
-        sort_list = list(range(1,10))
-        check_list= arr[:]
-        if check_list == sort_list:
-            return check_list is True
+    # 스도쿠 검사하는 플래그 생성
+    isClear = True
+
 
     # 가로 검사
     for i in range(9):
-        checklist(arr[i])
-        if True:
-            ans +=1
+        if checklist(arr[i]) == False:
+            isClear = False
+            break
     
     # 세로 검사
     for j in range(0, 9):
