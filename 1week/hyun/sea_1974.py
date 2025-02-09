a1dd20eb52b77a994a6c3f0225ebaef05c7ddb72
hyyuni <cjn112070@gmail.com>
2025-02-09 18:07:28 +0900

    [SEA] 파리 퇴치 / 2001 / D2 / 60분

--- 1week/hyun/sea_1974.py
+++ 1week/hyun/sea_1974.py
@@ -1,34 +1,43 @@
 # 스도쿠 퍼즐을 검사하기 위한 함수 생성
 def checklist(arr):
-    sort_list = list(range(1,10))
-    check_list = arr[:]
-    check_list.sort()
-    return True if sort_list == check_list else False
+    return sorted(arr) == list(range(1, 10))
 
 T = int(input())
-for tc in range(1, T+1):
-    # 스도쿠 퍼즐을 리스트 컴프리헨션으로 생성
-    arr = [list(map(int, input().split())) for _ in range(9)]
-    # 스도쿠 검사하는 플래그 생성
-    isClear = True
 
+for tc in range(1, T+1):
+    # 스도쿠 퍼즐 입력 받기
+    arr = [list(map(int, input().split())) for _ in range(9)]
+    # 스도쿠 검사하는 플래그
+    isClear = True
 
     # 가로 검사
     for i in range(9):
-        if checklist(arr[i]) == False:
+        if not checklist(arr[i]):
             isClear = False
             break
     
     # 세로 검사
-    for j in range(0, 9):
-        for i in range(0, 9):
-            checklist(arr[i][j])
-            if True:
-                ans +=1
+    if isClear:
+        for i in range(9):
+            a_list = [arr[j][i] for j in range(9)]
+            if not checklist(a_list):
+                isClear = False
+                break
 
-    if ans == 18:
-        print(1)
-    else:
-        print(0)
+    # 3×3 박스 검사
+    if isClear:
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                box = []
+                for x in range(3):
+                    for y in range(3):
+                        box.append(arr[i + x][j + y])
+                if not checklist(box):
+                    isClear = False
+                    break
+            if not isClear:
+                break
 
-    print(f'{tc} {ans}') 
+    # 결과 출력
+    ans = 1 if isClear else 0
+    print(f'#{tc} {ans}')
