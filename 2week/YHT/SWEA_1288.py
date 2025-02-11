bdbca942cf5c5415d5b3345c70df547245f9f2cc
Youtak2 <dbgudxkr1@naver.com>
2025-02-11 21:50:28 +0900

    [SWEA]새로운 불면증 치료/1288/53분

--- 2week/YHT/SWEA_1288.py
+++ 2week/YHT/SWEA_1288.py
@@ -1,28 +1,23 @@
-def find(l):
-    for x in range(100):
-        if l[99][x] == 2:
-            start_x = x
-            break
+def a(b, c):
+    d = []
+    for e in range(1, b + 1):
+        f = c[e - 1]
+        g = set()
+        h = 0
+        
+        while len(g) < 10:
+            h += 1
+            i = f * h
+            g.update(str(i))
+        
+        d.append(f"#{e} {i}")
     
-    dx = [-1, 1, 0]
-    dy = [0, 0, -1]
-    
-    y, x = 99, start_x
-    
-    while y > 0:
-        for d in range(2):  
-            nx, ny = x + dx[d], y + dy[d]
-            if 0 <= nx < 100 and l[ny][nx] == 1:
-                while 0 <= nx < 100 and l[ny][nx] == 1:
-                    x = nx
-                    nx += dx[d]
-                break  
-        y -= 1  
-    
-    return x
+    return d
 
-for _ in range(10):
-    test_case = int(input())
-    l = [list(map(int, input().split())) for _ in range(100)]
-    result = find(l)
-    print(f"#{test_case} {result}")
+# 입력 예시
+b = int(input())
+c = [int(input()) for _ in range(b)]
+
+# 결과 출력
+for j in a(b, c):
+    print(j)
