35757c16d99a18c13a1594a5c5f3c09d20079c3c
Youtak2 <dbgudxkr1@naver.com>
2025-02-15 20:21:41 +0900

    스터디 2주차 문제들

--- 2week/YHT/SWEA_1974.py
+++ 2week/YHT/SWEA_1974.py
@@ -1,19 +1,14 @@
-def a(board):
-    def b(unit):
-        return sorted(unit) == list(range(1, 10))
-    
-    for i in range(9):
-        if not b(board[i]) or not b([board[j][i] for j in range(9)]):
-            return 0
-    
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            if not b([board[x][y] for x in range(i, i+3) for y in range(j, j+3)]):
-                return 0
-    
-    return 1
-
 T = int(input())
+
 for t in range(1, T + 1):
-    board = [list(map(int, input().split())) for _ in range(9)]
-    print(f"#{t} {a(board)}")
+    N = int(input())
+    result = ""
+    
+    for _ in range(N):
+        C, K = input().split()
+        result += C * int(K)
+    
+    print(f"#{t}")
+    
+    for i in range(0, len(result), 10):
+        print(result[i:i+10])
